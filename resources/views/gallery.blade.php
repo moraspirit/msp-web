2a23601c020c8fefbcc13ee2b835077a0c45ea01
Sasitha Sonnadara <sasithawathmal@gmail.com>
2016-07-22 00:34:14 +0530

    updated gallery js

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -40,18 +40,18 @@
                 shareButton: true,
                 addThis: null,
                 photoChecked: function(photo){
-                    console.log("PHOTO CHECKED: " + photo.id + " - " + photo.url + " - " + photo.thumb);
-                    console.log("CHECKED PHOTOS COUNT: " + this.checkedPhotos.length);
+                    /*console.log("PHOTO CHECKED: " + photo.id + " - " + photo.url + " - " + photo.thumb);
+                    console.log("CHECKED PHOTOS COUNT: " + this.checkedPhotos.length);*/
                 },
                 photoUnchecked: function (photo) {
-                    console.log("PHOTO UNCHECKED: " + photo.id + " - " + photo.url + " - " + photo.thumb);
-                    console.log("CHECKED PHOTOS COUNT: " + this.checkedPhotos.length);
+                    /*console.log("PHOTO UNCHECKED: " + photo.id + " - " + photo.url + " - " + photo.thumb);
+                    console.log("CHECKED PHOTOS COUNT: " + this.checkedPhotos.length);*/
                 },
                 albumSelected: function (photo) {
-                    console.log("ALBUM CLICK: " + photo.id + " - " + photo.url + " - " + photo.thumb);
+//                    console.log("ALBUM CLICK: " + photo.id + " - " + photo.url + " - " + photo.thumb);
                 },
                 photoSelected: function (photo) {
-                    console.log("PHOTO CLICK: " + photo.id + " - " + photo.url + " - " + photo.thumb);
+//                    console.log("PHOTO CLICK: " + photo.id + " - " + photo.url + " - " + photo.thumb);
                 }
             });
 

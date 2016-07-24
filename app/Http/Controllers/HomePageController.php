cdaff7329712308166503d1d260ddc0cc3ecd6bc
ishankap2 <ishankaprasad587@gmail.com>
2016-07-23 21:01:12 -0700

    changed home controller

--- app/Http/Controllers/HomePageController.php
+++ app/Http/Controllers/HomePageController.php
@@ -66,11 +66,46 @@ class HomePageController extends Controller {
                 $logo2 = $sum2->logo;
                 $name2 = $sum2->uni_name;
             }
-            
+
+            $score_a = $summeryitem->t_a_score;
+            $index_a = $score_a[strlen($score_a)-1];
+            $i_a = strlen($score_a)-1;
+            while($index_a == '0') {
+                if ($score_a[$i_a] == '0') {
+                    $score_a[$i_a] = '';
+                    $i_a--;
+                }
+
+                $index_a = $score_a[$i_a];
+            }
+
+            //remove dot if no numbers exist after dot
+            $explode_a = explode('.', $score_a);
+            if (isset($explode_a[1]) && intval($explode_a[1]) <= 0) {
+                $score_a = intval($explode_a[0]);
+            }
+
+            $score_b = $summeryitem->t_b_score;
+            $index_b = $score_b[strlen($score_b)-1];
+            $i_b = strlen($score_b)-1;
+            while($index_b == '0') {
+                if ($score_b[$i_b] == '0') {
+                    $score_b[$i_b] = '';
+                    $i_b--;
+                }
+
+                $index_b = $score_b[$i_b];
+            }
+
+            //remove dot if no numbers exist after dot
+            $explode_b = explode('.', $score_b);
+            if (isset($explode_b[1]) && intval($explode_b[1]) <= 0) {
+                $score_b = intval($explode_b[0]);
+            }
 
            $summery [] = ['title'=>$summeryitem->heading, 'vs1'=> $summeryitem->t_a_code, 'vs2'=> $summeryitem->t_b_code,
                             'logo1'=>$logo1, 'logo2'=> $logo2 , 'dvs' => $name1.' '.'vs'.' '.$name2, 'won'=> $summeryitem->t_won,
-                            'summery'=> $summeryitem->summery, 't_a_score'=> $summeryitem->t_a_score,'t_b_score'=> $summeryitem->t_b_score
+                            'summery'=> $summeryitem->summery, 't_a_score'=> $score_a,'t_b_score'=> $score_b
 
             ];
         }

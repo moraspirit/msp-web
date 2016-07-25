b9244ea00a3b3d04bfeba7b542e2994a93749398
Sasitha Sonnadara <sasithawathmal@gmail.com>
2016-07-26 00:26:48 +0530

    Removed backend parts

--- config/database.php
+++ config/database.php
@@ -55,9 +55,9 @@ return [
 		'mysql' => [
 			'driver'    => 'mysql',
 			'host'      => env('DB_HOST', 'localhost'),
-			'database'  => env('DB_DATABASE', 'msp'),
-			'username'  => env('DB_USERNAME', 'root'),
-			'password'  => env('DB_PASSWORD', '12345'),
+			'database'  => env('DB_DATABASE', ''),
+			'username'  => env('DB_USERNAME', ''),
+			'password'  => env('DB_PASSWORD', ''),
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',

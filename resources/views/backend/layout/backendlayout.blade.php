6a3e6349b411625fd451b22ee22b1e4b13d8c0f7
Sasitha Sonnadara <sasithawathmal@gmail.com>
2016-07-26 22:47:41 +0530

    Small changes to Admin panel

--- resources/views/backend/layout/backendlayout.blade.php
+++ resources/views/backend/layout/backendlayout.blade.php
@@ -59,7 +59,7 @@
                 <!-- menu prile quick info -->
                 <div class="profile">
                     <div class="profile_pic">
-                        <img src="images/img.jpg" alt="..." class="img-circle profile_img">
+                        <img src="images/MSP HiQuality LOGO (1).png" alt="..." class="img-circle profile_img">
                     </div>
                     <div class="profile_info">
                         <span>Welcome,</span>

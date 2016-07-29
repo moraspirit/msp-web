8683404f184654fc200d4ccdcd85e0f62ae71632
Sasitha Sonnadara <sasithawathmal@gmail.com>
2016-07-29 15:21:16 +0530

    Added if check of Welcome text.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -279,10 +279,12 @@
     <!-- /.Top list -->
 
     {{--Welcome text to show until points are available--}}
+    @if(!isset($ovwomen) && !isset($ovmen) && !isset($ovall))
     <div class="container text-center font-oswald" style="padding-top: 35px;">
         <div style="font-size: 40px">WE ARE READY!</div>
         <div style="font-size: 25px">STAY WITH US FOR EXCITING STUFFS AND SCORES.</div>
     </div>
+    @endif
 
     <div class="container">
         <hr />

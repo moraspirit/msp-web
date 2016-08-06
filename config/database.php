8d36aaec6c7beba1db304e6039bfc686ecaa869a
unknown <tdiluksha@gmail.com>
2016-08-06 20:28:48 +0530

    Corrected db access changes to database.php

--- config/database.php
+++ config/database.php
@@ -54,14 +54,10 @@ return [
 
 		'mysql' => [
 			'driver'    => 'mysql',
-			/*'host'      => env('DB_HOST', 'localhost'),
+			'host'      => env('DB_HOST', 'localhost'),
 			'database'  => env('DB_DATABASE', ''),
 			'username'  => env('DB_USERNAME', ''),
-			'password'  => env('DB_PASSWORD', ''),*/
-			'host'      => 'localhost',
-			'database'  => 'msp',
-			'username'  => 'root',
-			'password'  => '12345',
+			'password'  => env('DB_PASSWORD', ''),
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
